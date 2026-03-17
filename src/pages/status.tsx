45c1ace3995cbd3238026aa3ca1e2940b56b7d99
ogd245 <oldgreatdemon@gmail.com>
2026-03-17 15:32:07 +0530

    od

--- src/pages/status.tsx
+++ src/pages/status.tsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-iimport { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/integrations/supabase/client";
 
 export default function AdminStatus() {
 
