3bd19fc9f8727fb0ed5cfd49242d6d13f118a88c
ogd245 <oldgreatdemon@gmail.com>
2026-03-17 15:06:24 +0530

    fix 404

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  "https://rltrfbamzscejztuhibg.supabase.co", // 👈 paste your real URL
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdHJmYmFtenNjZWp6dHVoaWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMTc4NDQsImV4cCI6MjA4ODc5Mzg0NH0.Pf3LWYxG1DwNBV6xksFgCCxWU8ESlqqFlAhKW5Mqmwk" // 👈 your anon key
 );
+
+export default supabase;
