27d76bf7d12baf6dee5ce9bdf489fa2b5eb7da4a
ogd245 <oldgreatdemon@gmail.com>
2026-03-17 14:40:10 +0530

    debug env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,2 @@
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
